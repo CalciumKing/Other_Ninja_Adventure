85810bf2b9cad0d8834e6a54054b15cff8b92cc6
CalciumKing <129754936+CalciumKing@users.noreply.github.com>
2024-05-14 14:11:49 -0400

    Stacking system
    stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack stack

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -18,10 +18,63 @@ public class Inventory : Singleton<Inventory>
     {
         if (Input.GetKeyDown(KeyCode.H))
         {
-            invItems[0] = testItem.CreateItem();
-            invItems[0].Quantity = 10;
+            AddItem(testItem, 15);
+        }
+    }
 
-            InventoryUI.i.DrawSlot(invItems[0], 0);
+    private void AddItem(InventoryItem item, int quantity)
+    {
+        if (item == null || quantity <= 0)
+            return;
+        List<int> itemIndexes = CheckItemStock(item.ID);
+        if(item.IsStackable && itemIndexes.Count > 0 )
+        {
+            foreach(int index in itemIndexes)
+            {
+                int currentMaxStack = item.MaxStack;
+                if (invItems[index].Quantity < currentMaxStack)
+                {
+                    invItems[index].Quantity += quantity;
+                    if (invItems[index].Quantity > currentMaxStack)
+                    {
+                        int diff = invItems[index].Quantity - currentMaxStack;
+                        invItems[index].Quantity = currentMaxStack;
+                        AddItem(item, diff);
+                    }
+                    InventoryUI.i.DrawSlot(invItems[index], index);
+                }
+            }
+        }
+        int quantityToAdd = (quantity > item.MaxStack) ? item.MaxStack : quantity;
+        AddItemToFreeSlot(item, quantityToAdd);
+        int remainingAmount = quantity - quantityToAdd;
+        if(remainingAmount > 0)
+            AddItem(item, remainingAmount);
+    }
+    private List<int> CheckItemStock(string itemID)
+    {
+        List<int> itemIndexes = new List<int>();
+        for (int i = 0; i < invItems.Length; i++)
+        {
+            if (invItems[i] == null)
+                continue;
+            if (invItems[i].ID == itemID)
+            {
+                itemIndexes.Add(i);
+            }
+        }
+        return itemIndexes;
+    }
+    private void AddItemToFreeSlot(InventoryItem item, int quantity)
+    {
+        for (int i = 0; i < invSize; i++)
+        {
+            if (invItems[i] != null)
+                continue;
+            invItems[i] = item.CreateItem();
+            invItems[i].Quantity = quantity;
+            InventoryUI.i.DrawSlot(invItems[i], i);
+            return;
         }
     }
 }
